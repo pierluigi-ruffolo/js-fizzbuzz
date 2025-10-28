00628a0fb8e521799dbd51738fa1ed2c0208baa2
pierluigi-ruffolo <pierluigiruffolo1@gmail.com>
2025-10-28 12:57:49 +0100

    first commit

---
+++ javascript/script.js
@@ -0,0 +1,7 @@
+/* cicliamo i numeri da 1 a 100 */
+/*  se un numero è divisibile per  3 e 5 stampiamo  FizzBuzz*/
+/*  se un numero è divisibile per  3  stampiamo  Fizz*/
+/* se un numero è divisibile per  5 stampiamo  buzz*/
+/* se il numero non è divisibile né per 3 né per 5 stampiamo il numero stesso */
+
+for (i = 1; i <= 100; i++) {}
