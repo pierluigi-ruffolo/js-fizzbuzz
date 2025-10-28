b108213139cad75640f4c1dca12b329673a625ed
pierluigi-ruffolo <pierluigiruffolo1@gmail.com>
2025-10-28 14:22:09 +0100

    feat: implemento  blocchi condizionali per gestire i casi nel ciclo

--- javascript/script.js
+++ javascript/script.js
@@ -4,4 +4,20 @@
 /* se un numero è divisibile per  5 stampiamo  buzz*/
 /* se il numero non è divisibile né per 3 né per 5 stampiamo il numero stesso */
 
-for (i = 1; i <= 100; i++) {}
+for (i = 1; i <= 100; i++) {
+  let numero = "";
+  if (i % 3 === 0 && i % 5 === 0) {
+    /*controlliamo se il numero è divisibile per   3 e 5 stampiamo  FizzBuzz */
+    numero = "fizz buzz";
+  } else if (i % 3 === 0) {
+    /*controlliamo se il numero è divisibile per   3 e  stampiamo  Fizz */
+    numero = "fizz";
+  } else if (i % 5 === 0) {
+    /*controlliamo se il numero è divisibile per   5 e  stampiamo  buzz */
+    numero = "buzz";
+  } else {
+    /* altrimenti stampiamo solo il numero */
+    numero = i;
+  }
+  console.log(numero);
+}
