b3faf0a92f161d523a24c70af12bc276019e656e
pierluigi-ruffolo <pierluigiruffolo1@gmail.com>
2025-10-28 17:39:37 +0100

    refactor: riscritta la logica FizzBuzz usando l’operatore ternario

--- javascript/script.js
+++ javascript/script.js
@@ -4,20 +4,28 @@
 /* se un numero è divisibile per  5 stampiamo  buzz*/
 /* se il numero non è divisibile né per 3 né per 5 stampiamo il numero stesso */
 
-let numero = "";
+/* let numero = "";
 for (let i = 1; i <= 100; i++) {
   if (i % 3 === 0 && i % 5 === 0) {
-    /*controlliamo se il numero è divisibile per   3 e 5 stampiamo  FizzBuzz */
     numero = "fizz buzz";
   } else if (i % 3 === 0) {
-    /*controlliamo se il numero è divisibile per   3 e  stampiamo  Fizz */
     numero = "fizz";
   } else if (i % 5 === 0) {
-    /*controlliamo se il numero è divisibile per   5 e  stampiamo  buzz */
     numero = "buzz";
   } else {
-    /* altrimenti stampiamo solo il numero */
     numero = i;
   }
+  console.log(numero);
+} */
+
+for (let i = 1; i <= 100; i++) {
+  let numero =
+    i % 3 === 0 && i % 5 === 0
+      ? "FizzBuzz"
+      : i % 3 === 0
+      ? "Fizz"
+      : i % 5 === 0
+      ? "Buzz"
+      : i;
   console.log(numero);
 }
